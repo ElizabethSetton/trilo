0827921751341e6179da771851c52db25dbe75d7
Elizabeth Setton <lizasali123@gmail.com>
2019-07-15 17:53:53 +0300

    changing port

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-    // chainWebpack: config => config.resolve.symlinks(false),
-    css: {
-      loaderOptions: {
-        sass: {
-          data: `@import "@/styles/_variables.scss";`
-        }
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 1111, // CHANGE YOUR PORT HERE!
+    https: false,
+    hotOnly: false,
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "@/styles/_variables.scss";`
       }
     }
-  };
+  }
+};
