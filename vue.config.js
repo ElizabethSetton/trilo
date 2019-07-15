c0a2b8ab33dd39920bfa6168efd3a6b3e4695166
Elizabeth Setton <lizasali123@gmail.com>
2019-07-15 17:59:51 +0300

    fixing devserver bug

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   devServer: {
-    open: process.platform === 'darwin',
     host: '0.0.0.0',
-    port: 1111, // CHANGE YOUR PORT HERE!
-    https: false,
-    hotOnly: false,
+    hot: true,
+    disableHostCheck: true,
   },
   css: {
     loaderOptions: {
